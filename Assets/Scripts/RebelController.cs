9c750a453444cc15c82e2a23a1a9d8ed4b9d8e1d
xtchhh <chappellxander77@gmail.com>
2026-06-21 22:20:36 -0400

    Added RebelController

---
+++ Assets/Scripts/RebelController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RebelController : MonoBehaviour
+{
+    public GameObject stormTrooper;
+    public NavMeshAgent rebelAI;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
