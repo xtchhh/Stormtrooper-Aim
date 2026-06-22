26d7dbdeff9cdcd37cadd818bdfdcdef868e10f9
xtchhh <chappellxander77@gmail.com>
2026-06-21 22:27:58 -0400

    AI logic

--- Assets/Scripts/RebelController.cs
+++ Assets/Scripts/RebelController.cs
@@ -14,6 +14,13 @@ public class RebelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float distanceToPlayer = Vector3.Distance(this.transform.position, stormTrooper.transform.position);
+
+        rebelAI.SetDestination(stormTrooper.transform.position);
     }
 }
